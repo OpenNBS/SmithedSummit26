5b61e7fcafc43835c5986c1eeff15569dd911780
Bentroen <29354120+Bentroen@users.noreply.github.com>
2026-08-10 01:19:12 -0300

    fix: tweak speaker ranges back to their original intended values

--- packages/playback/src/config.py
+++ packages/playback/src/config.py
@@ -22,13 +22,13 @@ SPEAKER_RANGES = [
     },
     {
         "name": "mid",
-        "outer_range": 24,
-        "inner_range": 20,
+        "outer_range": 20,
+        "inner_range": 16,
     },
     {
         "name": "long",
-        "outer_range": 40,
-        "inner_range": 32,
+        "outer_range": 32,
+        "inner_range": 24,
     },
 ]
 
